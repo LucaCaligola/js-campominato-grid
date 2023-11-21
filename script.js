0799d14fcfb7962cf45d019c9a9fc9497a1e8257
LucaCaligola <luca.caligola@gmail.com>
2023-11-21 16:15:37 +0100

    created a function to creare a new div and add the "square" class

--- script.js
+++ script.js
@@ -4,3 +4,26 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 // emetto un messaggio in console con il numero della cella cliccata.
+
+
+// prendiamo dove inserire i quadrati
+const mainContainer = document.getElementById("#content")
+
+// creaiamo una funzione per creare i quadrati
+
+function getSquare() {
+    const newSquareElement = document.createElement('div')
+    newSquareElement.classList.add('square');
+    return newSquareElement;
+}
+
+// crediamo un ciclo per inserire i quadrati e i numeri
+for (let i = 1; i <= 100; i++) {
+
+    
+    mainContainer.appendChild(getSquare())
+}
+
+
+
+
